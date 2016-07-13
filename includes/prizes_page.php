99e4dfcc412f82e738a0e1cccd7dd1c6d276b5bf
anzarsh <anzarsh@mail.ru>
2016-07-13 10:42:44 +0300

    шрифты

---
+++ includes/prizes_page.php
@@ -0,0 +1,20 @@
+
+<section class="inner-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="anz-red-boarder">
+					<span class="anz-title">Гарантированный приз</span>
+					<span class="anz-text">Каждый участник получает гарантированный приз:</span>
+					<span class="anz-text">50 руб. на мобильный телефон<strong>*</strong> за 5 зарегистрированных кодов<strong>**</strong></span>
+					<span class="anz-subtext">* Количество денежных вознаграждений на телефон - не более 9 шт.</span>
+					<span class="anz-subtext">** В неделю можно регистрировать до 5 кодов.</span>
+				</div>
+				<div class="anz-red-boarder">sdfgdfs</div>
+				<div class="anz-red-boarder">sdfgds</div>
+			</div>
+			<div class="clearfix"></div>	
+			
+		</div>
+	</div>
+</section>
