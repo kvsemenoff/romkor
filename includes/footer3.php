9a45dde324535b9824824fa3276888d5de608254
anzarsh <anzarsh@mail.ru>
2016-07-13 17:36:30 +0300

    футер2

---
+++ includes/footer3.php
@@ -0,0 +1,98 @@
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-12 col-xs-12">  
+				© Все права защищены. РОМКОР, 2016
+                <div>
+                    <a href="#"><img src="img/vk.png" alt=""></a>
+                    <a href="#"><img src="img/facebook.png" alt=""></a>
+                    <a href="#"><img src="img/ok.png" alt=""></a>
+                    <a href="#"><img src="img/instagram.png" alt=""></a>
+                </div>
+            </div>
+            <div class="col-md-9 col-sm-12 col-xs-12">
+            	<div class="footer-info">
+                    Информацию об организаторе акции, правилах её проведения, количестве призов по результатам акции, сроках, месте 
+                    и порядке их получения см. на сайте romkor-promo.ru или узнавайте по телефону горячей линии: 8-800-250-04-40. 
+                    Количество призов ограничено. Внешний вид призов может отличаться от их изображения.   
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div id="mask"></div>
+
+<!-- **************************** registration-form **************************** -->
+
+<!-- <div id="registration" class="window"> 
+    <form action="" class="common-form form1">
+        <div class="an-exit">
+            <span class="an-exit__krest"></span>
+        </div>
+        <span class="form-title">Регистрация</span>
+        <div class="row">
+            <span class="form-subtitle">Персональные данные</span>
+            <div class="col-md-6">
+                <input type="text" name="familiya" placeholder="Фамилия" class="familiya">
+                <input type="text" name="otchestvo" placeholder="Отчество" class="otchestvo">
+            </div>
+            <div class="col-md-6">
+                <input type="text" name="uname" placeholder="Имя"  class="ah-uname">
+                <input type="month" name="month" placeholder="Дата рождения" class="ah-month">
+            </div>
+            
+            <div class="clearfix"></div>
+
+            <span class="form-subtitle">Контактные данные</span>
+            <div class="col-md-6">
+                <input type="text" name="tel" placeholder="+7 900 000 00 00" class="phone" class="ah-tel">
+            </div>
+            <div class="col-md-6">
+                <input type="mail" name="mail" placeholder="E-mail" class="ah-mail">
+            </div>
+
+            <div class="clearfix"></div>
+
+            <span class="form-subtitle">Адрес</span>
+            <div class="col-md-12">
+                <input type="text" name="adres" placeholder="Cтрана, область, город" class="ah-adres">
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="col-md-6">
+                <select>
+                    <option disabled selected>Улица</option>
+                    <option>Улица1</option>
+                    <option>Улица2</option>
+                    <option>Улица3</option>
+                </select>
+                <input type="text" name="stroenie" placeholder="Строение" class="ah-stroenie">
+                <input type="text" name="pochta" placeholder="Почтовый индекс" class="ah-pochta">
+            </div>
+            <div class="col-md-6">
+                <input type="text" name="dom" placeholder="Дом" class="ah-dom">
+                <input type="text" name="kvartira" placeholder="Квартира" class="ah-kvartira">
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="col-md-12">
+                <input type="checkbox" class="otchestvo">
+                <span>Ознакомлен и согласен с Правилами Акции </span>
+                <input type="checkbox" class="otchestvo">
+                <p>Согласен на предоставление и обработку персональных данных (для их использования Организатором и уполномоченными им лицами в рамках настоящей Акции</p>
+                <input type="checkbox" class="otchestvo">
+                <span>Согласен  получать информационные сообщения в рамках настоящей Акции</span>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <a href="#">Зарегистрироваться</a>
+        </div>
+    </form>
+</div> -->
+
+<!-- **************************** registration-form **************************** -->
+
