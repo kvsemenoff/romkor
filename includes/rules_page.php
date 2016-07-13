b5f6d595cf5f192555de2a35ad4a469e42ecb524
hatuhov <hatuhov@yandex.ru>
2016-07-13 10:51:49 +0300

    ssssssssssssssssssss

--- includes/rules_page.php
+++ includes/rules_page.php
@@ -4,17 +4,31 @@
 		<div class="row">
 			<div class="col-md-4">
 				<div class="rules-item">
-					
+					<img src="img/r1.png" class="rules-item-img" alt="r1">
+					<img src="img/r4.png" class="rules-item-imgarrow" alt="r4">
+					<p class="rules-item-text">Покупайте продукцию ТМ РОМКОР* и регистрируйте коды с этикеток
+					на сайте romcor-promo.ru до 30 ноября.</p>
+					<span class="rules-item-span">*В акции участвует вся продукция, отмеченная символом “Акция! В отпуск на море!”</span>
+					<a href="#" class="rules-item-link">Список продукции</a>
+					<img src="img/rk1.png" class="rules-item-img" alt="rk1">
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="rules-item">
-					
+					<img src="img/r2.png" class="rules-item-img" alt="r2">
+					<img src="img/r4.png" class="rules-item-imgarrow" alt="r4">
+					<p class="rules-item-text">Каждую неделю: розыгрыш шашлычницы, набор посуды TEFAL, бутербродницы и гарантированный приз 
+					50 рублей за 5 кодов!</p>
+					<img src="img/rk2.png" class="rules-item-img" alt="rk2">
+					<img src="img/rk3.png" class="rules-item-img" alt="rk3">
+					<a href="#">Полные правила акции</a>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="rules-item">
-					
+					<img src="img/r3.png" class="rules-item-img" alt="r3">
+					<p class="rules-item-text">12 декабря розыгрыш путешествия iPhone 6S, холодильника с годовым запасом продуктов</p>
+					<img src="img/rk4.png" class="rules-item-img" alt="rk4">
 				</div>
 			</div>
 		</div>
